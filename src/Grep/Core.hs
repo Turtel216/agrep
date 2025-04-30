fe604a1af0dcbee6611bb9abf11a1ed1cfbd1e32
Turtel216 <papadimitrios216@gmail.com>
2025-04-30 13:24:39 +0300

    Update Grep.Core for readabilty and performace improvements

--- src/Grep/Core.hs
+++ src/Grep/Core.hs
@@ -6,63 +6,76 @@ import qualified System.Environment as Sys
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Text (Text)
-import System.Directory
-import Control.Exception
-import Data.Text.Internal.Search
+import System.Directory (doesFileExist, doesDirectoryExist, listDirectory)
+import Control.Exception (try)
+import Data.Text.Internal.Search (indices)
 import System.FilePath ((</>))
+import Control.Monad (forM_, unless)
 
+-- | Represents the type of location being searched
 data LocationType = DirectoryType | FileType deriving (Eq, Show)
 
-data SearchType = SearchType {
-    searchTerm :: Text
-  , searchLocation :: FilePath
-  , searchLocationType :: LocationType
-} deriving (Eq, Show)
+-- | Contains search parameters
+data SearchType = SearchType
+  { searchTerm :: !Text
+  , searchLocation :: !FilePath
+  , searchLocationType :: !LocationType
+  } deriving (Eq, Show)
 
--- TODO: Make less imperative
+-- | Parse command line arguments and validate the search location
 fetchVals :: IO (Either Text SearchType)
-fetchVals = do 
+fetchVals = do
   argList <- Sys.getArgs
-  if length argList < 2 then pure $ Left "Incorrect number of arguments.\nUsage: agrep [SEARCH TERM] [SEARCH LOCATION]" else do
-    let searchLocation = argList !! 1
-    fb <- doesFileExist searchLocation
-    db <- doesDirectoryExist searchLocation
-    if fb then pure $ Right $ SearchType (T.pack $ head argList) searchLocation FileType
-      else if db then pure $ Right $ SearchType (T.pack $ head argList) searchLocation DirectoryType
-      else pure $ Left $ "Invalid search location" <> T.pack searchLocation
+  case argList of
+    (term:location:_) -> validateLocation term location
+    _ -> pure $ Left "Incorrect number of arguments.\nUsage: agrep [SEARCH TERM] [SEARCH LOCATION]"
+  where
+    validateLocation :: String -> FilePath -> IO (Either Text SearchType)
+    validateLocation term location = do
+      isFile <- doesFileExist location
+      isDir <- doesDirectoryExist location
+      if isFile
+        then pure $ Right $ SearchType (T.pack term) location FileType
+        else if isDir
+          then pure $ Right $ SearchType (T.pack term) location DirectoryType
+          else pure $ Left $ "Invalid search location: " <> T.pack location
 
+-- | Main entry point
 main' :: IO ()
 main' = do
-  eRes <- fetchVals 
-  case eRes of 
-    Left e -> T.putStrLn e
-    Right r -> processSearching r
+  result <- fetchVals
+  case result of
+    Left err -> T.putStrLn err
+    Right searchParams -> processSearching searchParams
 
+-- | Process a search based on the provided search parameters
 processSearching :: SearchType -> IO ()
-processSearching SearchType{..} = do
+processSearching SearchType{..} =
   case searchLocationType of
     FileType -> searchFile searchTerm searchLocation
     DirectoryType -> do
       files <- listDirectory searchLocation
-      mapM_ (\file -> do
-                res <- doesDirectoryExist (searchLocation </> file)
-                if res then processSearching (SearchType searchTerm (searchLocation </> file) DirectoryType)
-                  else searchFile searchTerm (searchLocation </> file)) files
+      forM_ files $ \file -> do
+        let fullPath = searchLocation </> file
+        isDir <- doesDirectoryExist fullPath
+        if isDir
+          then processSearching (SearchType searchTerm fullPath DirectoryType)
+          else searchFile searchTerm fullPath
 
+-- | Search a single file for the search term
 searchFile :: Text -> FilePath -> IO ()
-searchFile searchTerm searchLocation = do
-  eContent <- try $ T.lines <$> T.readFile searchLocation :: IO (Either IOError [T.Text])
-  case eContent of
-    Left _ -> pure ()
-    Right content -> searchString searchTerm content
+searchFile searchTerm filePath = do
+  result <- try $ T.readFile filePath :: IO (Either IOError Text)
+  case result of
+    Left _ -> pure ()  -- Silently ignore files that can't be read
+    Right content -> searchInLines searchTerm (T.lines content)
 
-searchString :: Text -> [Text] -> IO ()
-searchString searchTerm content = searchString' searchTerm content 1
-
-searchString' :: Text -> [Text] -> Int -> IO ()
-searchString' _ [] _ = pure ()
-searchString' searchTerm (hayStack:remainingLines) lineNumber = do
-  case indices searchTerm hayStack of
-    [] -> searchString' searchTerm remainingLines (lineNumber + 1)
-    _ -> (T.putStrLn $ T.pack (show lineNumber) <> "\t" <> hayStack)
-      >> searchString' searchTerm remainingLines (lineNumber + 1)
+-- | Search for a term in lines of text and print matches with line numbers
+searchInLines :: Text -> [Text] -> IO ()
+searchInLines searchTerm = go 1
+  where
+    go _ [] = pure ()
+    go lineNum (line:rest) = do
+      unless (null $ indices searchTerm line) $
+        T.putStrLn $ T.pack (show lineNum) <> "\t" <> line
+      go (lineNum + 1) rest
